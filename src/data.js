92e5111f20a4928e44734827273772ae95f3a402
Emily Zhai <ezhai24@gmail.com>
2023-11-17 12:03:26 -0500

    fix eslint errors

--- src/data.js
+++ src/data.js
@@ -1,27 +1,43 @@
 import { Response, Server } from "miragejs";
 
-import projects from './data/projects';
-import tasks from './data/tasks';
-import dependencies from './data/dependencies';
+import dependencies from "./data/dependencies";
+import projects from "./data/projects";
+import tasks from "./data/tasks";
 
 new Server({
   routes() {
     this.namespace = "api";
 
-    this.get("/projects/", () => {
-      return projects;
-    }, {timing: 1000});
+    this.get(
+      "/projects/",
+      () => {
+        return projects;
+      },
+      { timing: 1000 }
+    );
 
-    this.get("/projects/:projectid/tasks/", (schema, request) => {
-      return tasks[request.params.projectid].map(t => {return {...t}}) || [];
-    }, {timing: 2000});
+    this.get(
+      "/projects/:projectid/tasks/",
+      (schema, request) => {
+        return (
+          tasks[request.params.projectid].map((t) => {
+            return { ...t };
+          }) || []
+        );
+      },
+      { timing: 2000 }
+    );
 
-    this.get("/projects/:projectid/dependencies", (schema, request) => {
-      if (dependencies.hasOwnProperty(request.params.projectid)) {
-        return dependencies[request.params.projectid];
-      } else {
-         return new Response(404, { errors: ["Project not found"]})
-      }
-    }, {timing: 2000});
-  }
+    this.get(
+      "/projects/:projectid/dependencies",
+      (schema, request) => {
+        if (dependencies[request.params.projectid]) {
+          return dependencies[request.params.projectid];
+        } else {
+          return new Response(404, { errors: ["Project not found"] });
+        }
+      },
+      { timing: 2000 }
+    );
+  },
 });
--- src/dependency_viz.jsx
+++ src/dependency_viz.jsx
@@ -1,39 +1,39 @@
 import { React } from "react";
 
 import "./dependency_viz.css";
-import api from './api';
+// import api from './api';
 
 export const DependencyVisualization = () => {
   return (
     <div className="depVizContainer">
       <h1>Project</h1>
       <select>
-        <option/>
+        <option />
         <option value="pid1">Placeholder Project Name 1</option>
         <option value="pid2">Placeholder Project Name 2</option>
       </select>
 
       <h2>Graph Stats</h2>
-        <table>
-          <tbody>
-            <tr>
-              <td>Task Count</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>Dependency Count</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>Root Count</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>Max Depth</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-        </table>
+      <table>
+        <tbody>
+          <tr>
+            <td>Task Count</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <td>Dependency Count</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <td>Root Count</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <td>Max Depth</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+      </table>
 
       <h2>Graph Visualization</h2>
       <div>Graph Viz Component</div>
